9633481970df31e0171d9542f6a6972118abb51f
titu <titu@verifyrocket.com>
2016-12-16 23:56:13 +0600

    ftp implementation -- test phase

--- application/controllers/User_controller.php
+++ application/controllers/User_controller.php
@@ -554,11 +554,12 @@ fa fa-cog fa-spin fa-3x fa-fw
 
                 );
 
-                $this->console_log('ftphost: '+$user->ftphost);
-                $this->console_log('username: '+$user->username);
-                $this->console_log('ftppassword: '+$user->ftppassword);
-                $this->console_log('$contactfile: '+$contactfile);
-                $this->console_log('$name: '+$name);
+                $this->console_log($user->ftphost);
+                $this->console_log('ftphost: '.$user->ftphost);
+                $this->console_log('username: '.$user->username);
+                $this->console_log('ftppassword: '.$user->ftppassword);
+                $this->console_log('$contactfile: '.$contactfile);
+                $this->console_log('$name: '.$name);
 
                 $uploadToFtp = $this->uploadToFTP($user->ftphost, $user->username, $user->ftppassword, $contactfile, $name);
 
