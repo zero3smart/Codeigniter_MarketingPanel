67176b769694a9636e8fe8af03f6361b49880aaf
titu <titu@verifyrocket.com>
2017-01-11 03:18:17 +0600

    code formatting

--- application/controllers/User_controller.php
+++ application/controllers/User_controller.php
@@ -19,9 +19,7 @@ class User_controller extends CI_Controller
             $actual_link = "http://$_SERVER[HTTP_HOST]$_SERVER[REQUEST_URI]";
 
             $this->session->set_flashdata(array('request' => $actual_link));
-            if(strpos($actual_link, "http://$_SERVER[HTTP_HOST]/activate") === false) {
-                redirect('login');
-            }
+            redirect('login');
         }
         $this->load->model("Mdl_user");
         $this->connection = new MongoClient("mongodb://127.0.0.1:27017");
@@ -281,23 +279,6 @@ class User_controller extends CI_Controller
 
 
         $this->load->view("user/dashboard", $data);
-    }
-
-    public function activate_account($activation_token)
-    {
-        /*$data['view']['page_title'] = 'Purchase';
-        $data['view']['page_sub_title'] = 'Monthly Packages';
-        $data['view']['section'] = 'package_stripe_form';
-        $data['view']['menu'] = 'store';
-        $data['view']['submenu'] = 'packages';
-        $data['view']['msg'] = $this->session->flashdata('msg');
-        $data['view']['msg_type'] = $this->session->flashdata('msg_type');
-        $data['package_document'] = $this->Mdl_user->fetch_package_single($package_id);
-
-        $this->load->view("user/dashboard", $data);*/
-        $data['view']['section'] = 'activation_token';
-        $data['activation_token'] = $activation_token;
-        $this->load->view("user/activate_account", $data);
     }
 
     public function packages()
