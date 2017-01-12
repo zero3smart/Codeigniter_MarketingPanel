7fb96d2e4898bcb1fa3edbe92d46b88d634246ff
titu <titu@verifyrocket.com>
2017-01-12 21:20:28 +0600

    code formatting

--- application/controllers/Stripe_payment.php
+++ application/controllers/Stripe_payment.php
@@ -127,7 +127,7 @@ class Stripe_payment extends CI_Controller
                 \Stripe\Stripe::setApiKey("sk_test_uf7di1vW51TKwuBaceT54rwm");
 
                 $customer = \Stripe\Customer::create(array(
-                    "description" => "Customer for " . $this->session->email_lookup_user_username,
+                    "description" => "Customer for " . $dash_profile,
                     "plan" => $package_id,
                     "card" => $_POST['stripeToken']
                 ));
