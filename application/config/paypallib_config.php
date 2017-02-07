ba1c69c45703af6674998146a52adf2a788b49c4
Titu <email.to.titu@gmail.com>
2017-02-07 21:33:48 +0600

    Paypal config api user/pass/signature updated

--- application/config/paypallib_config.php
+++ application/config/paypallib_config.php
@@ -14,10 +14,10 @@ $config['paypal_lib_button_path'] = 'buttons';
 // What is the default currency?
 $config['paypal_lib_currency_code'] = 'USD';
 
-$config['Sandbox'] = TRUE;
+$config['Sandbox'] = FALSE;
 
-$config['APIUsername'] = $config['Sandbox'] ? 'developer1_api1.client.com' : 'PRODUCTION_USERNAME_GOES_HERE';
-$config['APIPassword'] = $config['Sandbox'] ? 'C85VJSTMGDKDSLLN' : 'PRODUCTION_PASSWORD_GOES_HERE';
-$config['APISignature'] = $config['Sandbox'] ? 'AFcWxV21C7fd0v3bYYYRCpSSRl31A-gCfR3a9kPX66JEDDIdyH18bCtW' : 'PRODUCTION_SIGNATURE_GOES_HERE';
+$config['APIUsername'] = $config['Sandbox'] ? 'developer1_api1.client.com' : 'carnerjames_api1.gmail.com';
+$config['APIPassword'] = $config['Sandbox'] ? 'C85VJSTMGDKDSLLN' : 'EBMSD56PYZE8CD3N';
+$config['APISignature'] = $config['Sandbox'] ? 'AFcWxV21C7fd0v3bYYYRCpSSRl31A-gCfR3a9kPX66JEDDIdyH18bCtW' : 'ABxSD615fLZE6bP75i-ULKnyon1DAUjm6c37QKKBjXnxkX5sXYp-88ZR';
 
 ?>
