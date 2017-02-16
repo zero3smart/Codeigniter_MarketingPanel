adde64a6a3c6f0f9bf3553eca617599492506014
Titu <email.to.titu@gmail.com>
2017-02-16 10:51:39 +0600

    http = https set for google font apis

--- application/views/user/file_upload_status_chart.php
+++ application/views/user/file_upload_status_chart.php
@@ -1,5 +1,5 @@
 
-        <link href="http://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Open+Sans:400,300,600,700&subset=all" rel="stylesheet" type="text/css" />
         <link href="<?php echo base_url();?>assets/global/plugins/font-awesome/css/font-awesome.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo base_url();?>assets/global/plugins/simple-line-icons/simple-line-icons.min.css" rel="stylesheet" type="text/css" />
         <link href="<?php echo base_url();?>assets/global/plugins/bootstrap/css/bootstrap.min.css" rel="stylesheet" type="text/css" />
