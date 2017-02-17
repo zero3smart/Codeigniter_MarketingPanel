e202a0488e40692b71231d0e9705a9786d5991be
Titu <email.to.titu@gmail.com>
2017-02-18 00:53:48 +0600

    we're secured already

--- application/config/config.php
+++ application/config/config.php
@@ -23,7 +23,7 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 | a PHP script and you can easily do that on your own.
 |
 */
-$config['base_url'] = 'http://app.verifyrocket.com/';
+$config['base_url'] = 'https://app.verifyrocket.com/';
 //$config['base_url'] = 'http://54.187.12.50/test.verifyrocket.com/';
 
 /*
